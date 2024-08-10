1870fc885fbea57aa8fcb69b53bcd0b3f8017476
Farrel Dinarta <farrel.dinarta@ruparupa.com>
2024-08-11 05:23:14 +0700

    migration for skill table

---
+++ database/data/migrations/20240810200127_game_skill.js
@@ -0,0 +1,28 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+    return knex.schema.createTable('game_skill', table => {
+        table.uuid('id').primary().notNullable().unique().defaultTo(knex.fn.uuid());
+        table.string('name').notNullable();
+        table.text('description').notNullable();
+        table.boolean('for_self').notNullable();
+        table.boolean('for_enemy').notNullable();
+        table.integer('radius_x').notNullable();
+        table.integer('radius_y').notNullable();
+        table.boolean('auto_trigger').notNullable();
+        table.integer('duration').notNullable();
+        table.integer('usage_count').notNullable();
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.timestamp('updated_at').defaultTo(knex.fn.now());
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+    return knex.schema.dropTableIfExists('game_skill');
+};
---
+++ database/data/seeds/game_skill.js
@@ -0,0 +1,78 @@
+const { v4 : uuid } = require('uuid');
+
+exports.seed = async function(knex) {
+  // Deletes ALL existing entries
+  await knex('game_skill').del()
+  await knex('game_skill').insert([
+        {
+          id: uuid(),
+          name: 'Enlightened Apprentice',
+          description: 'Enables pawn to move like king.',
+          for_self: true,
+          for_enemy: false,
+          radius_x: 1,
+          radius_y: 1,
+          auto_trigger: false,
+          duration: 0, // unlimited
+          usage_count: 4,
+          created_at: knex.fn.now(),
+          updated_at: knex.fn.now()
+      },
+      {
+          id: uuid(),
+          name: 'The Great Wall',
+          description: 'Creates wall to block movement.',
+          for_self: false,
+          for_enemy: false,
+          radius_x: 3,
+          radius_y: 3,
+          auto_trigger: false,
+          duration: 4,
+          usage_count: 2,
+          created_at: knex.fn.now(),
+          updated_at: knex.fn.now()
+      },
+      {
+          id: uuid(),
+          name: 'Fog Master',
+          description: 'Hides half board vision for enemy',
+          for_self: true,
+          for_enemy: false,
+          radius_x: 7,
+          radius_y: 7,
+          auto_trigger: true,
+          duration: 3,
+          usage_count: 1,
+          created_at: knex.fn.now(),
+          updated_at: knex.fn.now()
+      }, 
+      {
+        id: uuid(),
+        name: 'Freezing Wand',
+        description: 'Disable enemy piece movement',
+        for_self: false,
+        for_enemy: true,
+        radius_x: 0,
+        radius_y: 0,
+        auto_trigger: false,
+        duration: 1,
+        usage_count: 3,
+        created_at: knex.fn.now(),
+        updated_at: knex.fn.now()
+      },
+      {
+        id: uuid(),
+        name: 'Paralyzer',
+        description: 'Disable enemy from using skills',
+        for_self: false,
+        for_enemy: true,
+        radius_x: 0,
+        radius_y: 0,
+        auto_trigger: true,
+        duration: 2,
+        usage_count: 1,
+        created_at: knex.fn.now(),
+        updated_at: knex.fn.now()
+      },
+  ]);
+};
